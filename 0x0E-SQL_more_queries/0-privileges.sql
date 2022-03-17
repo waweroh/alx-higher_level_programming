4a8630745e9e0b0019b99c22a728da2cdc4b6411
waweroh <scadmsez@gmail.com>
2022-03-16 17:34:07 -0700

    script that lists all privileges of the MySQL users

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
 -- Lists priviledges for user_0d_1
 SHOW GRANTS FOR user_0d_1@localhost;
 -- Lists priviledges for user_0d_2
-SHOW GRANTS FOR user_0d_2@llocalhost;
+SHOW GRANTS FOR user_0d_2@localhost;
