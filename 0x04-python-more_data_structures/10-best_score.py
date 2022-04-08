d88a3d0c92e5fe0e8589a7deea44070870e73df9
waweroh <scadmsez@gmail.com>
2022-04-08 02:27:12 -0700

    corrections on data stuctures

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python3
+
+
 def best_score(a_dictionary):
-    if not a_dictionary:
-        return None
+    max_key = None
+    if a_dictionary is not None:
+        keys = list(a_dictionary.keys())
+        max_num = a_dictionary.get(keys[0])
+        max_key = keys[0]
+        for i in keys:
+            if a_dictionary.get(i) > max_num:
+                max_num = a_dictionary.get(i)
+                max_key = i
+
+        return max_key
