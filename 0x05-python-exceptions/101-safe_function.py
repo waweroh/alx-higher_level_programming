6e2031c4ce936a73aecec5fb22f912f1c60ef905
waweroh <scadmsez@gmail.com>
2022-04-08 21:33:10 -0700

    advanced tasks

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import sys
+def safe_function(fct, *args):
+    try:
+        r = fct(*args)
+        return r
+    except Exception as e:
+        sys.stderr.write("Exception: %s\n" % e)
+        return None
--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -4,7 +4,7 @@
 def safe_print_list_integers(my_list=[], x=0):
     j = 0
 
-    for i in range(x):
+    for i in range(0, x):
         try:
             print("{:d}".format(my_list[i]), end="")
             j = j + 1
