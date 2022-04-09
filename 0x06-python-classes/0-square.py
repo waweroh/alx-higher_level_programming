616428ba4d87758e4161524427323937f63a60fd
waweroh <scadmsez@gmail.com>
2022-04-09 02:24:09 -0700

    classes and objects

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Class Square definition"""
+
+
+class Square:
+    """an empty class called Square that defines a square"""
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Defines a class Square"""
+
+
+class Square:
+    """This is definition of class square.
+
+    Attributes:
+        __size: Size of the square
+
+    Args:
+        size: The size of the square
+    """
+    def __init__(self, size):
+        self.__size = size
