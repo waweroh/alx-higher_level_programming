1ca13810183c03848f9a6ccd1f01875b65118d29
waweroh <scadmsez@gmail.com>
2022-04-09 03:53:08 -0700

    corrections

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,4 +1,2 @@
--- Lists the number of records with the same score in the table second_table of
-the database hbtn_0c_0
-SELECT score, COUNT(*) AS number FROM second_table GROUP BY score ORDER BY numb
-er DESC;
+-- List by occurences of scores
+SELECT score, COUNT(*) AS number FROM second_table GROUP BY score ORDER BY number DESC;
