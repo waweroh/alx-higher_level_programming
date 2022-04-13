61b60f6333df278d3ffbfa05ed7e5b63487c7003
waweroh <scadmsez@gmail.com>
2022-04-14 02:12:30 +0300

    standard errors

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,7 +2,7 @@
 if (process.argv.length < 3) {
   console.log('No argument');
 } else if (process.argv.length === 3) {
-  console.log('Argument found');  
+  console.log('Argument found');
 } else {
   console.log('Argument found');
 }
