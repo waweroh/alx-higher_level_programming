c5d7d4a9ac6c87b0cd09eaef64cbfc5bacf46db3
waweroh <scadmsez@gmail.com>
2022-04-30 05:32:19 +0300

    corrections

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -5,6 +5,8 @@ import sys
 
 
 if __name__ == "__main__":
-    r = requests.get(sys.argv[1])
-    ri = r.headers.get('X-Request-Id')
-    print(ri)
+    try:
+        r = requests.get(sys.argv[1])
+        print(r.headers['X-Request-Id'])
+    except:
+        pass
--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -5,6 +5,6 @@ import sys
 
 
 if __name__ == "__main__":
-    email = {'email': sys.argv[2]}
-    r = requests.post(sys.argv[1], data=email)
+    r = requests.post(sys.argv[1], data={'email': sys.argv[2]})
     print(r.text)
+    
