eeeb16f4ce6919953a01b8f0da5c7a4156330ce0
waweroh <scadmsez@gmail.com>
2022-09-23 05:44:17 +0300

    request server to respond witha message

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# sends request to server to display specific message
-curl -sX PUT -L --max-redirs -1 -d "user_id=98" -H "Origin: You got me!" 0.0.0.0:5000/catch_me
+# Sends request to server to display specific message
+curl -sX PUT -L -F "user_id=98" -H "Origin: You got me!" 0.0.0.0:5000/catch_me
