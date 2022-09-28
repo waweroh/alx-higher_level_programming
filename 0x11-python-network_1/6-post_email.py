207efc9df66929676de317d880ac93d495497796
waweroh <scadmsez@gmail.com>
2022-09-28 04:56:51 +0300

    passed URL with the email as a parameter

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,9 @@
+""" post email """
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    email = {'email': sys.argv[2]}
+    req = requests.post(sys.argv[1], data=email)
+    print(req.text)
