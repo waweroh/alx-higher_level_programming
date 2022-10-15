c880bcd7698f7f5eed39e57bff7997d240c941c0
waweroh <scadmsez@gmail.com>
2022-10-15 20:46:43 +0300

    add test function

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -4,14 +4,16 @@ This module 0-add_integer suppliesthe function add_integer()
 returning sum of int type only
 """
 
+
 def add_integer(a, b=98):
     """ Returns the addition of a and b, an exact integer."""
+
     if type(a) not in (int, float):
         raise TypeError("a must be an integer")
     if type(b) not in (int, float):
         raise TypeError("b must be an integer")
-
     return int(a) + int(b)
+
 
 if __name__ == "__main__":
     import doctest
