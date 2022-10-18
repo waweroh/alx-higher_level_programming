dbd1412c29d6aa234e99b9f058398ab021d0866e
waweroh <scadmsez@gmail.com>
2022-10-18 10:13:05 +0300

    returns if there is an arg or not

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// print arguments passed
+
+if (process.argv.length < 3) {
+  console.log('No arguments');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Argument found');
+}
