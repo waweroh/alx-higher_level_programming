6650bd3eb879780656bfdb711a49f4669591bf74
waweroh <scadmsez@gmail.com>
2022-10-18 11:13:23 +0300

    returns if there is an arg or not

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 // print arguments passed
 
-if (process.argv.length === 2) {
-  console.log('No arguments');
-} else if (process.argv.length === 3) {
-  console.log('Argument found');
+if (process.argv.length < 3) {
+    console.log('No arguments');
+} else if (process.argv.length === 3) { 
+    console.log('Argument found');
 } else {
-  console.log('Arguments found');
+    console.log('Arguments found');
 }
