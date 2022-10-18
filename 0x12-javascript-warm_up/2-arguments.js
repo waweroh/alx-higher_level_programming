014c52c380d9f02ab747ff7321688c60fec579dc
waweroh <scadmsez@gmail.com>
2022-10-18 11:40:22 +0300

    print arguments found

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,10 +2,9 @@
 // arguments with Javascript
 
 if (process.argv.length < 3) {
-    console.log('No argument');
-  } else if (process.argv.length === 3) {
-    console.log('Argument found');
-  } else {
-    console.log('Arguments found');
-  }
-  
+  console.log('No argument');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
